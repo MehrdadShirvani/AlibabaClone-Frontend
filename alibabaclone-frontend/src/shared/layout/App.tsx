3386eeeddd7a6bb5f9597eab22744ca358d89518
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-27 15:47:12 +0330

    fix: remove unncessary codes in App.tsk

--- alibabaclone-frontend/src/shared/layout/App.tsx
+++ alibabaclone-frontend/src/shared/layout/App.tsx
@@ -1,18 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import agent from "../api/agent";
-import CityDropdown from "@/features/city/CityDropdown";
 import TransportationSearchForm from "@/features/transportation/transportationSearchForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Button>Click me</Button>
       <h1>Transportation Search</h1>
       <div className="card">
         <div>
