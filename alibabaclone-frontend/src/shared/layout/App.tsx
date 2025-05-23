4c6a6de2aafd45005c3b2e492f9849325dcf20f3
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-23 20:39:00 +0330

    feat: add navbar to App.tsx

--- alibabaclone-frontend/src/shared/layout/App.tsx
+++ alibabaclone-frontend/src/shared/layout/App.tsx
@@ -1,13 +1,13 @@
+import Navbar from "@/components/navbar";
 import "./App.css";
 import TransportationSearchForm from "@/features/transportation/transportationSearchForm";
 
 function App() {
   return (
     <>
-      <h1>Transportation Search</h1>
       <div className="card">
+        <Navbar />
         <div>
-          <h2>Choose the transportation search form</h2>
           <TransportationSearchForm />
         </div>
       </div>
