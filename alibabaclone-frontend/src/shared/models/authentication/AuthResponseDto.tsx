3f79fe0b138c520376bce2b22452ba81bcfb2a57
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-30 20:52:35 +0330

    feat: add authentication models

---
+++ alibabaclone-frontend/src/shared/models/authentication/AuthResponseDto.tsx
@@ -0,0 +1,5 @@
+export interface AuthResponseDto {
+  token: string;
+  phoneNumber: string;
+  roles: string[];
+}
---
+++ alibabaclone-frontend/src/shared/models/authentication/RegisterRequestDto.tsx
@@ -0,0 +1,5 @@
+export interface RegisterRequestDto {
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+}
