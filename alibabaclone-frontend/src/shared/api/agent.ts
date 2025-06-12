775ac4f1deea8111732db2a966be61a916704c8d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-13 00:04:37 +0330

    feat: use axios request interceptor to attach token and handle logout add endpoints for profile

--- alibabaclone-frontend/src/shared/api/agent.ts
+++ alibabaclone-frontend/src/shared/api/agent.ts
@@ -5,6 +5,15 @@ import { City } from '../models/location/city';
 import { RegisterRequestDto } from '../models/authentication/RegisterRequestDto';
 import { LoginRequestDto } from '../models/authentication/LoginRequestDto';
 import { AuthResponseDto } from '../models/authentication/AuthResponseDto';
+import { ProfileDto } from '../models/account/ProfileDto';
+import { EditEmailDto } from '../models/account/EditEmailDto';
+import { EditPasswordDto } from '../models/account/EditPasswordDto';
+import { PersonDto } from '../models/account/PersonDto';
+import { UpsertBankAccountDetailDto } from '../models/account/UpsertBankAccountDetailDto';
+import { TravelerTicketDto } from '../models/transportation/TravelerTicketDto';
+import { TicketOrderSummaryDto } from '../models/transportation/TicketOrderSummaryDto';
+import { useAuthStore } from '@/store/authStore';
+import { TransactionDto } from '../models/transaction/TransactionDto';
 
 axios.defaults.baseURL = 'https://localhost:44377/api';
 
@@ -16,6 +25,42 @@ const request = {
     put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     delete: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
+
+
+axios.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axios.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout(); // TODO: redirect
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+const Profile = {
+  getProfile: () => request.get<ProfileDto>('/account/profile'),
+  editEmail: (data: EditEmailDto) => request.put<void>('/account/email', data),
+  editPassword: (data: EditPasswordDto) => request.put<void>('/account/password', data),
+  upsertAccountPerson: (data: PersonDto) => request.post<number>('/account/account-person', data),
+  upsertPerson: (data: PersonDto) => request.post<number>('/account/person', data),
+  upsertBankDetail: (data: UpsertBankAccountDetailDto) =>
+    request.post<void>('/account/bank-detail', data),
+  getMyPeople: () => request.get<PersonDto[]>('/account/my-people'),
+  getMyTravels: () => request.get<TicketOrderSummaryDto[]>('/account/my-travels'),
+  getTravelOrderDetails: (ticketOrderId: number) =>
+    request.get<TravelerTicketDto[]>(`/account/my-travels/${ticketOrderId}`),
+  getMyTransactions: () => request.get<TransactionDto[]>('/account/my-transactions'), 
+};
+
 
 const TransportationSearch = {
     search: (data: TransportationSearchRequest) => request.post<TransportationSearchResult[]>('/transportation/search', data),
@@ -32,6 +77,7 @@ register: (data: RegisterRequestDto) =>
 };
 
 const agent = {
+    Profile,
     TransportationSearch,
     Cities,
     Auth
