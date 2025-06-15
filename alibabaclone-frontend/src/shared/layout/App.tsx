c4ddc19fbc87af79eeab76981fc25e8747f8c8df
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-06-15 11:52:44 +0330

    feat: adjust Routes

--- alibabaclone-frontend/src/shared/layout/App.tsx
+++ alibabaclone-frontend/src/shared/layout/App.tsx
@@ -5,6 +5,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "@/features/transportation/pages/SearchPage";
 import SearchResultsPage from "@/features/transportation/pages/SearchResultsPage";
 import ProfilePage from "@/features/account/ProfilePage";
+import TravelOrderDetails from "@/features/account/TravelOrderDetails";
+import MyTravels from "@/features/account/MyTravels";
+import AccountInfo from "@/features/account/AccountInfo";
+import ListOfTravelers from "@/features/account/ListOfTravelers";
+import Favorites from "@/features/account/Favorites";
+import MyTransactions from "@/features/account/MyTransactions";
+import Support from "@/features/account/Support";
 
 function App() {
   return (
@@ -19,7 +26,15 @@ function App() {
             path="/:vehicleId/:fromCityId/:toCityId"
             element={<SearchResultsPage />}
           />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />}>
+            <Route path="account-info" element={<AccountInfo />} />
+            <Route path="my-travels" element={<MyTravels />} />
+            <Route path="my-travels/:id" element={<TravelOrderDetails />} />
+            <Route path="list-of-travelers" element={<ListOfTravelers />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="support" element={<Support />} />
+            <Route path="transactions" element={<MyTransactions />} />
+          </Route>
         </Routes>
       </div>
     </Router>
